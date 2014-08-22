744f6301c0dfcc47ef8617430178bb5feb550db2
NTF-32 <ntf-32@NTF-32.(none)>
2014-08-22 17:08:39 +0530

    dfg

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, '683066385912-i279sdnha8n28r0j6gudqsoi8eu73qgt.apps.googleusercontent.com', 'JfWkfMgrrKa7Gi5gGfj54aN4'
+  provider :google_oauth2, '683066385912-i279sdnha8n28r0j6gudqsoi8eu73qgt.apps.googleusercontent.com', 'JfWkfMgrrKa7Gi5gGfj54aN4', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
 end
